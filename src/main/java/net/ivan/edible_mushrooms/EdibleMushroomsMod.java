2e4be1b9addb9f6aff5bf24c067abe399c375b58
Blasty6000 <ivanasen77@gmail.com>
2024-11-14 21:31:16 +0200

    Initial commit

---
+++ src/main/java/net/ivan/edible_mushrooms/EdibleMushroomsMod.java
@@ -0,0 +1,16 @@
+package net.ivan.edible_mushrooms;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EdibleMushroomsMod implements ModInitializer {
+	public static final String MOD_ID = "ediblemushrooms";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ivan/edible_mushrooms/edibleMushroomModClient.java
@@ -0,0 +1,10 @@
+package net.ivan.edible_mushrooms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class edibleMushroomModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
